705f98fb98cb1a4b33fc5b934e4c4d0ae7d85b85
kunal <kunalgulati98@gmail.com>
2020-08-17 01:31:18 -0700

    transfered navigation and home page files

---
+++ client/components/NavigationBar.js
@@ -0,0 +1,322 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Divider from '@material-ui/core/Divider'
+
+/** GraphQl Query */
+import gql from 'graphql-tag';
+import { useQuery } from "@apollo/react-hooks";
+import Router from "next/router";
+
+const GET_CART_ITEM_QUANTITY = gql`
+  query itemQuantityList($userId: ObjectId) {
+    getOrderCartItems(userId: $userId) {
+      quantity,
+    }
+  }
+`;
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    '& > *': {
+      margin: theme.spacing(1),
+    }, // for button spacing
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
+// var CartItemCount = (id) => {
+//   const { data, loading, error } = useQuery(GET_CART_ITEM_QUANTITY,
+//     {
+//       variables: {
+//         userId: id
+//       }
+//     }
+//   );
+
+//   // if (loading) return console.log("Loading");
+//   if (error) return <p>ERROR</p>;
+//   if (!data) return <p>Not found</p>;
+//   var count = 0;
+//   for (var i = 0; i < data.getOrderCartItems.length; i++) {
+//     count += data.getOrderCartItems[i].quantity;
+//   };
+
+//   return count;
+// };
+
+export default function NavigatinBar(props) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMenuProfileClick = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+    Router.push({
+      pathname: '/profile',
+    })
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleForkchaClick = () => {
+    Router.push({
+      pathname: '/',
+    })
+  };
+
+  const handleLogout = () => {
+    Router.push({
+      pathname: '/logout',
+    })
+  }
+
+  /** GraphQL Hooks */
+  const getCartQuantity = (currentUserId) => {
+    // const { loading, error, data } = useQuery(GET_CART_ITEM_QUANTITY, {
+    //   variables: { userId: currentUserId },
+    // });
+    // if (loading) return <p>Loading ...</p>;
+    // if (error) {
+    //   // return  `Error! ${ error.message } `;
+    //   return 0
+    // }
+    // if (!data) return 0;
+
+    // let count = 0;
+    // for (let i = 0; i < data.getOrderCartItems.length; i++) {
+    //   count += data.getOrderCartItems[i].quantity;
+    // }
+
+    // return count;
+    return 0;
+  }
+
+  // eslint-disable-next-line react/prop-types
+  const cartValue = getCartQuantity(props.userId);
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem><Typography variant="subtitle2">kunal Gulati (Name TODO)</Typography></MenuItem>
+      <MenuItem><Typography variant="caption">kunal@forkcha.com (Email TODO)</Typography></MenuItem>
+      <Divider />
+      <MenuItem onClick={handleMenuProfileClick}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Help</MenuItem>
+      <Divider />
+      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit" href="/orderSummary">
+          <Badge badgeContent={cartValue} color="secondary">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+        <p>Shopping Cart</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  // when using drawer, Need to change Nav bar Position to "fixed", otherwise, keep "relative"
+  let navbarPosition = "relative";
+  // eslint-disable-next-line react/prop-types
+  if (props.position === undefined) {
+    navbarPosition = "relative";
+  } else {
+    // eslint-disable-next-line react/prop-types
+    navbarPosition = props.position;
+  }
+
+  return (
+    <div className={classes.grow}>
+      {/* <AppBar position="relative" className={classes.appBar}> */}
+      <AppBar position={navbarPosition} className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+          </IconButton>
+          <MenuItem onClick={handleForkchaClick}> <Typography className={classes.title} variant="h6" noWrap> ForkCha </Typography> </MenuItem>
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+
+            <Button href="/marketplace"> Marketplace</Button>
+            <IconButton aria-label="show 4 new mails" color="inherit" href="/orderSummary">
+              {/* CART */}
+              <Badge badgeContent={cartValue} color="secondary">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+}
---
+++ client/components/home/ProductHero.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import {Typography} from '@material-ui/core';
+import ProductHeroLayout from './ProductHeroLayout';
+import { Grid } from '@material-ui/core';
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1508004727890-1f5e1a22932e?auto=format&fit=crop&w=1400&q=80'
+
+const styles = (theme) => ({
+  root: {
+    color: '#ffffff',
+  },
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+    backgroundColor: '#2fbc84',
+    color: 'inherit'
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(4),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+  buttonGrid: {
+    flexGrow: 1,
+    width: '80%',
+    padding: theme.spacing(4),
+  }
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+  // const [spacing, setSpacing] = React.useState(2);
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" /> */}
+      <Typography color="inherit" align="center" variant="h3" marked="center">
+        Waste Less, Make More Profit
+      </Typography>
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Source Rescued Food from Forkcha for your Production Unit
+      </Typography>
+
+      <Grid container justify="center" spacing={3}>
+        <Grid item>
+          <Button
+            color="default"
+            variant="contained"
+            size="large"
+            className={classes.button}
+            component="a"
+            href="/buyer/register"
+          >
+            Register
+            </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            color="default"
+            variant="contained"
+            size="large"
+            className={classes.button}
+            component="a"
+            href="/buyer/login"
+          >
+            Login
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            color="default"
+            variant="contained"
+            size="large"
+            className={classes.button}
+            component="a"
+            href="/buyer/marketplace"
+          >
+            Marketplace
+          </Button>
+        </Grid>
+      </Grid>
+      <Typography variant="body2" color="inherit" className={classes.more}>
+        Discover the experience
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
